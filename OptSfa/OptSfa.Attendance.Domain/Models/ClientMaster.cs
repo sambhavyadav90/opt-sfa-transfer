c96993af11c32ff733e3a59779e4b2e3d64ff54e
Sambhav <sambhavyadav90@gmail.com>
2025-09-30 10:33:14 +0530

    partial project setup has been done

---
+++ OptSfa/OptSfa.Attendance.Domain/Models/ClientMaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptSfa.Attendance.Domain.Models;
+
+public class ClientMaster
+{
+    public int client_id { get; set; }
+    public string client_name { get; set; }
+}
---
+++ OptSfa/OptSfa.Migration.Data/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OptSfa.Migration.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptSfa.Migration.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<ClientMaster> DbClientMaster => Set<ClientMaster>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ClientMaster>(eb =>
+        {
+            eb.HasKey(e => e.client_id);
+            eb.Property(e => e.client_name).HasMaxLength(200).IsRequired();
+        });
+    }
+}
